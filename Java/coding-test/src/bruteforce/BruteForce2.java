80f755a7a6d2db7da630505f9989eec035d8f0c0
DuhanMo <devduhanmo@gmail.com>
2021-09-21 00:59:06 +0900

    add: BruteForce1 백준 15651 중복O 순열 BruteForce2 백준 15649 중복X 순열 BruteForce3 백준 15652 중복O 조합 BruteForce4 백준 15650 중복X 조합

--- Java/coding-test/src/bruteforce/BruteForce2.java
+++ Java/coding-test/src/bruteforce/BruteForce2.java
@@ -3,11 +3,16 @@ package bruteforce;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class Basic {
+/**
+ * 백준 15649
+ * 중복X 순서대로 나열
+ */
+public class BruteForce2 {
 
     static int N;
     static int M;
     static int[] selected;
+    static int[] used;
     static StringBuilder sb = new StringBuilder();
 
     static void input() {
@@ -15,31 +20,32 @@ public class Basic {
         N = scan.nextInt();
         M = scan.nextInt();
         selected = new int[M + 1];
+        used = new int[N + 1];
     }
 
-    //Recurrence Function (재귀 함수)
-    //만약 M 개를 전부 고름   => 조건에 맞는 탐색을 한 가지 성공한 것!
-    //아직 M 개를 고르지 않음 => k 번째부터 M번째 원소를 조건에 맞게 고르는 모든 방법을 시도한다.
-    static void rec_func(int k) {
-        if (k == M + 1) { //다 골랐다!
+    static void recFunc(int k) {
+        if (k == M + 1) {
             for (int i = 1; i <= M; i++) {
                 sb.append(selected[i]).append(" ");
             }
             sb.append("\n");
         } else {
             for (int cand = 1; cand <= N; cand++) {
+                if (used[cand] == 1) {
+                    continue;
+                }
                 selected[k] = cand;
-                rec_func(k + 1);
+                used[cand] = 1;
+                recFunc(k + 1);
                 selected[k] = 0;
+                used[cand] = 0;
             }
         }
     }
 
     public static void main(String[] args) {
         input();
-
-        //1 번째 원소부터 M 번째 원소를 조건에 맞는 모든 방법을 찾아줘
-        rec_func(1);
+        recFunc(1);
         System.out.println(sb.toString());
     }
 
---
+++ Java/coding-test/src/bruteforce/BruteForce4.java
@@ -0,0 +1,90 @@
+package bruteforce;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 15650
+ * 중복X 고르기
+ */
+public class BruteForce4 {
+
+    static int N;
+    static int M;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+
+    static void input() {
+        FastReader scan = new FastReader();
+        N = scan.nextInt();
+        M = scan.nextInt();
+        selected = new int[M + 1];
+    }
+
+    static void recFunc(int k) {
+        if (k == M + 1) {
+            for (int i = 1; i <= M; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+        } else {
+            for (int cand = selected[k - 1] + 1; cand <= N; cand++) {
+                selected[k] = cand;
+                recFunc(k + 1);
+                selected[k] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        recFunc(1);
+        System.out.println(sb.toString());
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
